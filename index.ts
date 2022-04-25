992eb83ac9056b6172b960cc0782f395fae7ef1b
Alex Rothberg <agrothberg@gmail.com>
2022-04-25 17:44:58 -0400

    tracking with db

--- index.ts
+++ index.ts
@@ -7,18 +7,39 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+import { Sequelize, DataTypes } from 'sequelize';
+
+const sequelize = new Sequelize('sqlite::memory:');
+const Tracker = sequelize.define('Tracker', {
+  trackId: DataTypes.STRING,
+});
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-app.get('/image.gif', (req: Request, res: Response) => {
+app.get('/image.gif', async (req: Request, res: Response) => {
   console.log(JSON.stringify(req.headers));
   console.log(JSON.stringify(req.query));
   console.log(req.ip);
-  res.sendFile(path.join(__dirname, '../responses', 'transparent.gif'))
+
+    if(req.query.trackId){
+      await Tracker.create({
+        trackId: req.query.trackId,
+      });
+    }
+
+  res.sendFile(path.join(__dirname, '../responses', 'transparent.gif'));
 });
 
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  await sequelize.sync();
   console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+app.get('/info', async (req: Request, res: Response) => {
+  const trackers = await Tracker.findAll();
+  console.log(trackers);
+  res.send(JSON.stringify(trackers));
 });
