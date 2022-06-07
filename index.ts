ccbee96f1622ea16b8066c468994cb68119d7124
Alex Rothberg <agrothberg@gmail.com>
2022-06-07 13:49:41 -0400

    kill old login flow

--- index.ts
+++ index.ts
@@ -324,7 +324,7 @@ app.post(
   }
 );
 
-app.get("/login", (req: Request, res: Response): void => {
+app.get("/magic-login", (req: Request, res: Response): void => {
   res.status(200).send("Logging in...");
   return;
 });
@@ -371,7 +371,7 @@ app.post(
       },
     });
 
-    const loginUrl = `${protocol}://${host}/magic?token=${magicLinkToken.token}`;
+    const loginUrl = `${protocol}://${host}/magic-login?token=${magicLinkToken.token}`;
 
     const msg = {
       to: user.email,
@@ -461,67 +461,6 @@ app.post(
         emailAccount: email,
         trackingSlug: slug,
       });
-    return;
-  }
-);
-
-// TODO(cancan): make this a POST that returns this information.
-// The GET should just be an empty page
-app.get(
-  "/magic",
-  query("token").isString().isUUID(),
-  async (req: Request, res: Response): Promise<void> => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
-      return;
-    }
-
-    const token = req.query.token as string;
-
-    const magicLinkToken = await prisma.magicLinkToken.findFirst({
-      where: { token },
-      include: { user: { select: { email: true, slug: true } } },
-    });
-
-    if (!magicLinkToken) {
-      res.status(400).json({ error_code: "token_invalid" });
-      return;
-    } else if (magicLinkToken.usedAt) {
-      res.status(400).json({ error_code: "token_used" });
-      return;
-    } else if (magicLinkToken.expiresAt < dayjs().toDate()) {
-      res.status(400).json({ error_code: "token_used" });
-      return;
-    }
-
-    await prisma.magicLinkToken.update({
-      where: { id: magicLinkToken.id },
-      data: {
-        usedAt: dayjs().toDate(),
-      },
-    });
-
-    const userId = magicLinkToken.userId;
-    const subject = String(userId);
-    const { email, slug } = magicLinkToken.user;
-
-    const expiresIn = ACCESS_TOKEN_EXPIRES_HOURS * 60 * 60;
-
-    const accessToken = await jsonwebtoken.sign(
-      {},
-      env.JWT_ACCESS_TOKEN_SECRET,
-      {
-        algorithm: JWT_ALGORITHM,
-        expiresIn,
-        subject,
-      }
-    );
-
-    res.redirect(
-      `/login#accessToken=${accessToken}&expiresIn=${expiresIn}&emailAccount=${email}&trackingSlug=${slug}`
-    );
     return;
   }
 );
