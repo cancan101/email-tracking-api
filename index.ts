2695d972e8a73e2281a0ad0a533077f8de4f4b31
Alex Rothberg <agrothberg@gmail.com>
2022-06-09 15:05:02 -0400

    Enable sentry (#19)

--- index.ts
+++ index.ts
@@ -16,6 +16,34 @@ import { expressjwt, ExpressJwtRequestUnrequired } from "express-jwt";
 import sgMail from "@sendgrid/mail";
 import { cleanEnv, str, email, port, num } from "envalid";
 import fs from "fs";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
+// -------------------------------------------------
+
+const app = express();
+
+// -------------------------------------------------
+
+Sentry.init({
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 // -------------------------------------------------
 
@@ -57,7 +85,6 @@ const JWT_ALGORITHM = "HS256";
 // -------------------------------------------------
 
 const prisma = new PrismaClient();
-const app = express();
 
 // -------------------------------------------------
 
@@ -494,6 +521,9 @@ app.post(
 app.get("/ping", (req: Request, res: Response): void => {
   res.status(200).send("");
 });
+
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
 
 // -------------------------------------------------
 
