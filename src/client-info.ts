b3cc769e21a72340f90ddffef55afe0e05c30c89
Alex Rothberg <agrothberg@gmail.com>
2022-08-02 17:21:03 -0400

    Break up files (#105)

---
+++ src/client-info.ts
@@ -0,0 +1,174 @@
+import * as Sentry from "@sentry/node";
+
+import { fetchWithTimeout } from "./utils";
+
+// -------------------------------------------------
+
+const EMAIL_PROVIDER_GMAIL = "Gmail";
+const EMAIL_PROVIDER_YAHOO = "Yahoo";
+const EMAIL_PROVIDER_FRONT_APP = "FrontApp";
+const EMAIL_PROVIDER_APPLE_MAIL = "Apple Mail";
+const EMAIL_PROVIDER_SUPERHUMAN = "Superhuman";
+
+async function lookupIpwhois(clientIp: string): Promise<ClientIpGeo | null> {
+  let clientIpGeo: ClientIpGeo | null = null;
+  const resp = await fetchWithTimeout(`http://ipwho.is/${clientIp}`);
+  clientIpGeo = { source: "ipwhois" };
+  if (resp.ok) {
+    const clientIpGeoData = await resp.json();
+
+    clientIpGeo.dataRaw = clientIpGeoData;
+
+    // e.g. rate limited
+    if (!clientIpGeoData.success) {
+      return clientIpGeo;
+    }
+
+    const isp = clientIpGeoData?.connection?.isp;
+
+    const isGoogleLlc = isp === "Google LLC";
+    // https://developer.apple.com/support/prepare-your-network-for-icloud-private-relay/
+    const isCloudflareInc = isp === "Cloudflare, Inc.";
+
+    if (isGoogleLlc) {
+      clientIpGeo.rule = "connectionIspGoogleLlc";
+      clientIpGeo.emailProvider = EMAIL_PROVIDER_GMAIL;
+    } else if (isCloudflareInc) {
+      clientIpGeo.rule = "connectionIspCloudflareInc";
+    } else {
+      clientIpGeo.data = {
+        city: clientIpGeoData.city as string,
+        region: clientIpGeoData.region as string,
+        regionCode: clientIpGeoData.region_code as string,
+        country: clientIpGeoData.country as string,
+        countryCode: clientIpGeoData.country_code as string,
+      };
+    }
+  } else {
+    const respJson = await resp.json();
+    Sentry.captureException(
+      new Error(
+        `Unable to fetch IP geo data ${resp.status}: ${JSON.stringify(
+          respJson
+        )}`
+      )
+    );
+  }
+  return clientIpGeo;
+}
+
+async function lookupIpApi(clientIp: string): Promise<ClientIpGeo | null> {
+  let clientIpGeo: ClientIpGeo | null = null;
+  const resp = await fetchWithTimeout(
+    `http://ip-api.com/json/${clientIp}?fields=status,message,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,isp,org,as,asname,mobile,proxy,hosting`
+  );
+  clientIpGeo = { source: "ip-api" };
+  if (resp.ok) {
+    const clientIpGeoData = await resp.json();
+
+    clientIpGeo.dataRaw = clientIpGeoData;
+
+    if (clientIpGeoData.status !== "success") {
+      return clientIpGeo;
+    }
+
+    const isp = clientIpGeoData?.isp;
+
+    const isGoogleLlc = isp === "Google LLC";
+    // https://developer.apple.com/support/prepare-your-network-for-icloud-private-relay/
+    const isCloudflareInc = isp === "Cloudflare, Inc.";
+
+    const org = clientIpGeoData?.org;
+    const isICloudPrivateRelay = org === "iCloud Private Relay";
+
+    if (isGoogleLlc) {
+      clientIpGeo.rule = "connectionIspGoogleLlc";
+      clientIpGeo.emailProvider = EMAIL_PROVIDER_GMAIL;
+    } else if (isICloudPrivateRelay) {
+      clientIpGeo.rule = "orgICloudPrivateRelay";
+      clientIpGeo.emailProvider = EMAIL_PROVIDER_APPLE_MAIL;
+      // The data should be reliable here
+      clientIpGeo.data = {
+        //TODO: factor this out as a helper:
+        city: clientIpGeoData.city as string,
+        region: clientIpGeoData.regionName as string,
+        regionCode: clientIpGeoData.region as string,
+        country: clientIpGeoData.country as string,
+        countryCode: clientIpGeoData.countryCode as string,
+      };
+    } else if (isCloudflareInc) {
+      clientIpGeo.rule = "connectionIspCloudflareInc";
+      // This is probably icloud.
+      // TODO: resolve to icloud vs not
+    } else {
+      clientIpGeo.data = {
+        city: clientIpGeoData.city as string,
+        region: clientIpGeoData.regionName as string,
+        regionCode: clientIpGeoData.region as string,
+        country: clientIpGeoData.country as string,
+        countryCode: clientIpGeoData.countryCode as string,
+        isMobile: clientIpGeoData.mobile as boolean,
+      };
+    }
+  } else {
+    const respJson = await resp.json();
+    Sentry.captureException(
+      new Error(
+        `Unable to fetch IP geo data ${resp.status}: ${JSON.stringify(
+          respJson
+        )}`
+      )
+    );
+  }
+  return clientIpGeo;
+}
+
+export async function getClientIpGeo(
+  clientIp: string,
+  userAgent: string | undefined
+): Promise<ClientIpGeo | null> {
+  let clientIpGeo: ClientIpGeo | null = null;
+
+  const isProxiedGoogle =
+    userAgent !== undefined && userAgent.includes("GoogleImageProxy");
+
+  const isProxiedYahoo =
+    userAgent !== undefined && userAgent.includes("YahooMailProxy");
+
+  const isProxiedFront =
+    userAgent !== undefined && userAgent.includes("FrontApp.com ImageProxy");
+
+  const isProxiedSuperhuman =
+    userAgent !== undefined && userAgent === "Superhuman";
+
+  const isProxied =
+    isProxiedGoogle || isProxiedYahoo || isProxiedFront || isProxiedSuperhuman;
+
+  if (isProxied) {
+    let emailProvider = undefined;
+    if (isProxiedGoogle) {
+      emailProvider = EMAIL_PROVIDER_GMAIL;
+    } else if (isProxiedYahoo) {
+      emailProvider = EMAIL_PROVIDER_YAHOO;
+    } else if (isProxiedFront) {
+      emailProvider = EMAIL_PROVIDER_FRONT_APP;
+    } else if (isProxiedSuperhuman) {
+      emailProvider = EMAIL_PROVIDER_SUPERHUMAN;
+    }
+
+    clientIpGeo = { source: "userAgent", emailProvider };
+  } else {
+    try {
+      clientIpGeo = await lookupIpApi(clientIp);
+      const clientIpGeoSecondary = await lookupIpwhois(clientIp);
+      if (clientIpGeo === null) {
+        clientIpGeo = clientIpGeoSecondary;
+      } else {
+        clientIpGeo.secondary = clientIpGeoSecondary ?? undefined;
+      }
+    } catch (error) {
+      Sentry.captureException(error);
+    }
+  }
+  return clientIpGeo;
+}
