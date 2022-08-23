52254aedf89f45f6e79fcd48a8f82e846019aef2
Alex Rothberg <agrothberg@gmail.com>
2022-08-23 17:06:44 -0400

    Use throng for cluster management (#121)
    * Use throng for cluster management
    
    * log WEB_CONCURRENCY

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
+import throng from "throng";
+
 import env from "./settings";
 import { app } from "./app";
 
-app.listen(env.PORT, async () => {
-  console.log(`[server]: Server is running on ${env.PORT}`);
-});
+function start() {
+  app.listen(env.PORT, async () => {
+    console.log(`[server]: Server is running on ${env.PORT}`);
+  });
+}
+
+console.log(`WEB_CONCURRENCY = ${env.WEB_CONCURRENCY}`);
+if (env.WEB_CONCURRENCY === 0) {
+  start();
+} else {
+  console.log("Using throng");
+  throng({
+    workers: env.WEB_CONCURRENCY,
+    lifetime: Infinity,
+    start,
+  });
+}
